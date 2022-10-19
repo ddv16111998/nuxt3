7fbc9e85f938cdcc231faded05230b3170c2358a
viendd <viendd@vmodev.com>
2022-10-19 15:59:55 +0700

    add vuevalidate

--- app.vue
+++ app.vue
@@ -3,5 +3,35 @@
     <el-button>Button</el-button>
     <el-button type="primary" class="main__start">adadadasd</el-button>
     <el-button type="primary">Primary</el-button>
+    <el-button type="primary" :icon="Edit" circle />
   </div>
 </template>
+
+<script>
+import {useVuelidate} from "@vuelidate/core";
+import {email, required} from "@vuelidate/validators";
+
+export default{
+  setup () {
+    return { v$: useVuelidate() }
+  },
+  data () {
+    return {
+      firstName: '',
+      lastName: '',
+      contact: {
+        email: ''
+      }
+    }
+  },
+  validations () {
+    return {
+      firstName: { required },
+      lastName: { required },
+      contact: {
+        email: { required, email }
+      }
+    }
+  }
+}
+</script>
